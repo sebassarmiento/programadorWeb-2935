7668cd6f4ab9f03bfefc7552c2457f129bacfa34
adrianc4 <adrian_c4@hotmail.com>
2018-05-31 15:21:27 -0300

    Add resolution Clase 01

---
+++ Clase 02/Profe/En clase/js/index.js
@@ -0,0 +1,21 @@
+// 1. Condicionales
+// - ìf
+// - switch
+// 2. Operadores de comparación
+// - ===
+// - !==
+// - <
+// - >
+// - >=
+// - <=
+// 3. Operadores lógicos
+// - !
+// - &&
+// - ||
+// 4. Ciclos e iterciones
+// - for
+// - while
+// - do while
+// 5. Sentencias
+// - break
+// - continue

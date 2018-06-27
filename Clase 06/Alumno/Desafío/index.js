4dd2632761141fbddd46395dae17a5418d6a95fa
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2018-06-27 18:13:30 -0300

    Fix Desafio Clase 06

--- Clase 06/Alumno/Desafío/index.js
+++ Clase 06/Alumno/Desafío/index.js
@@ -1,15 +1,18 @@
 function includesText (string1, string2) {
-  if (
-    typeof string1 === 'string' &&
-    typeof string2 === 'string' &&
-    string1.toUpperCase().indexOf(string2.toUpperCase())
-  ) {
-    return true
+  if (typeof string1 === 'string' && typeof string2 === 'string') {
+    var string1UpperCase = string1.toUpperCase()
+    var string2UpperCase = string2.toUpperCase()
+    if(string2UpperCase.indexOf(string1UpperCase)!== -1){
+      return true
+    }else{
+      return false
+    }
   } else {
     return false
   }
 }
 
 console.log(includesText('Pa', 'Patricia'))
+console.log(includesText('pa', 'Patricia'))
 console.log(includesText('Patricia', 'Pa'))
 console.log(includesText(2, 'Pa'))

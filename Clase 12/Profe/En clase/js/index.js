e9f94a75b9583c2cf3661328e0f7de575b1dccc3
adrianc4 <adrian_c4@hotmail.com>
2018-07-05 20:58:44 -0300

    Add clase 12

---
+++ Clase 12/Profe/En clase/js/index.js
@@ -0,0 +1,112 @@
+console.log('Init app')
+
+// var titleNode = document.getElementById('title')
+
+// console.log(titleNode)
+
+// var titleJQNode = $('#title')
+
+// console.log(titleJQNode)
+
+// var listNodes = document.getElementsByClassName('list')
+
+// for (var i = 0; i < listNodes.length; i++) {
+//   listNodes[i].innerHTML = 'Pato'
+// }
+
+// console.log(listNodes)
+
+// var listJQNodes = $('.list')
+
+// listJQNodes.html('Patitos')
+
+// $('.list.change').html('Patitos')
+
+// console.log(listJQNodes)
+
+// var listJQNodes = $('.list.change')
+
+// listJQNodes.addClass('red')
+// var titleJQNode = $('#title')
+
+// setTimeout(function () {
+//   titleJQNode.removeClass('red')
+// }, 3000)
+
+// var listJQNodes = $('li')
+
+// listJQNodes.click(removeSelf)
+
+// function removeSelf (event) {
+//   var liJQNode = $(this)
+//   liJQNode.addClass('red')
+//   console.log('Hicieron un click')
+// }
+
+// var inputJQNodes = $('input')
+
+// inputJQNodes.blur(showMessageBlur)
+
+// function showMessageBlur (event) {
+//   console.log('Hicieron un blur')
+// }
+
+// var titleJQNode = $('#title')
+
+// titleJQNode.click(changeColor)
+
+// function changeColor (event) {
+//   var h1JQNode = $(this)
+
+//   if (h1JQNode.hasClass('red')) {
+//     h1JQNode.removeClass('red')
+//   } else {
+//     h1JQNode.addClass('red')
+//   }
+
+//   console.log('Hicieron un click')
+// }
+
+// var liNodes = document.getElementsByTagName('li')
+
+// for (var i = 0; i < liNodes.length; i++) {
+//   liNodes[i].onclick = changeColor
+// }
+
+// var listJQNodes = $('li')
+
+// listJQNodes.click(changeColor)
+
+// function changeColor (event) {
+//   var liJQNode = $(this)
+
+//   if (liJQNode.hasClass('red')) {
+//     liJQNode.removeClass('red')
+//   } else {
+//     liJQNode.addClass('red')
+//   }
+
+//   console.log('Hicieron un click')
+// }
+
+// var inputJQNodes = $('input')
+
+// inputJQNodes.on('input', showMessage)
+
+// function showMessage () {
+//   var inputJQNode = $(this)
+
+//   var value = inputJQNode.val()
+
+//   console.log('Blur', value)
+// }
+
+var deleteButtonJQNode = $('#deleteButton')
+
+deleteButtonJQNode.click(clearFields)
+
+function clearFields () {
+  var inputJQNodes = $('[type=text]')
+
+  inputJQNodes.val('Pato')
+}

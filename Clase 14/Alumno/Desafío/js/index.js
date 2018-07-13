3785ac461ec6545ad4189a1c1947b1048da41ef5
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2018-07-13 14:05:06 -0300

    Desafío Clase 14

--- Clase 14/Alumno/Desafío/js/index.js
+++ Clase 14/Alumno/Desafío/js/index.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $('button').on('click', removeElementWithAnimation)
-    function removeElementWithAnimation(){
-        $('#1').hide(2000, function(){
-            $('#1').remove()
+    function removeElementWithAnimation(event){
+        $(this).parent().parent().hide(2000, function(){
+            $(this).remove()
         })
     }
 })
+
